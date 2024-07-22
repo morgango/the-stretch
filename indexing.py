f24a3242d07fb2130389a4d0de9c8bad6948f033
Morgan Goeller <morgango@gmail.com>
2024-07-22 11:44:50 -0500

    Synonyms working, UI better

--- indexing.py
+++ indexing.py
@@ -4,78 +4,103 @@ import glob
 import os
 import hashlib
 import re
+import csv
+
 
 from decouple import config
 elastic_cloud_id = config('ELASTIC_CLOUD_ID', default='none')
 elastic_api_key = config('ELASTIC_API_KEY', default='none')
 elastic_index_name = config('ELASTIC_INDEX_NAME', default='none')
 raw_data = config('RAW_DATA', default='none')
+elastic_synonym_fn = config('ELASTIC_SYNONYM_FILE', default='none')
+elastic_synonym_id = config('ELASTIC_SYNONYM_ID', default='none')
 
-def create_index_with_fields():
+elastic_client = Elasticsearch(
+    cloud_id=elastic_cloud_id,
+    api_key=elastic_api_key,
+)
 
-    # Assign the provided Elasticsearch cloud ID and API key to local variables
-    cloud_id = elastic_cloud_id
-    api_key = elastic_api_key
-    index_name = elastic_index_name
+def read_synonyms_from_csv(synonyms_fn = elastic_synonym_fn):  
     
-    # Create an Elasticsearch client using the provided cloud ID and API key
-    client = Elasticsearch(
-        cloud_id=cloud_id,
-        api_key=api_key
-    )
+    count = 0
+    synonyms_set = []
 
-    # Define the mappings for the index
-    # The mappings specify the fields in the documents and their types
-    # mapping = {
-    #     "mappings": {
-    #         "properties": {
-    #             "file_name": { "type": "text" },
-    #             "line_number": { "type": "integer" },
-    #             "heading": { "type": "text" },
-    #             "text": { "type": "text" },
-    #             "text_completion": { "type": "completion" }
-    #         }
-    #     }
-    # }
+    with open(synonyms_fn, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            count += 1
+            synonym_dict = {}
+            synonym_dict["id"] = "synonym-{}".format(count)
+            synonym_dict["synonyms"] = line.strip()
+            synonyms_set.append(synonym_dict)
 
-    mapping = {
-        "settings": {
-            "analysis": {
-                "filter": {
-                    "autocomplete_filter": {
-                        "type": "edge_ngram",
-                        "min_gram": 1,
-                        "max_gram": 10
-                    }
+    return synonyms_set
+
+
+def create_synonyms_with_csv(client = elastic_client, 
+                             synonyms_fn= elastic_synonym_fn, 
+                             synonyms_id=elastic_synonym_id):
+    
+    synonyms_set = read_synonyms_from_csv(synonyms_fn=synonyms_fn)
+    client.synonyms.put_synonym(id=synonyms_id, synonyms_set=synonyms_set)
+
+def create_index_with_fields(client = elastic_client, 
+                             index_name=elastic_index_name):
+
+    settings = {
+        "analysis": {
+            "filter": {
+                "autocomplete_filter": {
+                    "type": "edge_ngram",
+                    "min_gram": 1,
+                    "max_gram": 10
                 },
-                "analyzer": {
-                    "autocomplete": {
-                        "type": "custom",
-                        "tokenizer": "standard",
-                        "filter": [
-                            "lowercase",
-                            "autocomplete_filter"
-                        ]
-                    }
+                "acme_synonym_filter": {
+                    "type": "synonym_graph",
+                    "synonyms_set": elastic_synonym_id,
+                    "updateable": True,
                 }
-            }
-        },
-        "mappings": {
-            "properties": {
-                "file_name": { "type": "text" },
-                "line_number": { "type": "integer" },
-                "heading": { "type": "text" },
-                "heading_completion": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "search_analyzer": "standard"
+            },
+            "analyzer": {
+                "autocomplete": {
+                    "type": "custom",
+                    "tokenizer": "standard",
+                    "filter": [
+                        "lowercase",
+                        "autocomplete_filter",
+                    ]
                 },
-                "text": { "type": "text" },
-                "text_completion": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "search_analyzer": "standard"
+                "acme_synonym_analyzer": {
+                    "tokenizer": "standard",
+                    "filter": [
+                        "lowercase",
+                        "acme_synonym_filter",
+                    ]
                 }
+            },
+        }
+    }
+
+    mappings = {
+        "properties": {
+            "file_name": { "type": "text" },
+            "line_number": { "type": "integer" },
+            "heading": { "type": "text" },
+            "heading_completion": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "search_analyzer": "standard"
+            },
+            "text": { "type": "text" },
+            "text_completion": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "search_analyzer": "standard"
+            },
+            "text_synonym": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "search_analyzer": "acme_synonym_analyzer"
             }
         }
     }
@@ -85,18 +110,13 @@ def create_index_with_fields():
         client.indices.delete(index=index_name)
         ic("Deleted index {}".format(index_name))
 
-    client.indices.create(index=index_name, body=mapping)
+    client.indices.create(index=index_name, mappings=mappings, settings=settings)
     ic("Created index {}".format(index_name))
 
-def index_file_to_elasticsearch(file_path: str):
-    cloud_id = elastic_cloud_id
-    api_key = elastic_api_key
-    index_name = elastic_index_name
-
-    client = Elasticsearch(
-        cloud_id=cloud_id,
-        api_key=api_key
-    )
+def index_file_to_elasticsearch(file_path: str, 
+                                client = elastic_client, 
+                                index_name=elastic_index_name):
+    
     last_heading = None  # This will keep track of the last seen heading
 
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -118,23 +138,33 @@ def index_file_to_elasticsearch(file_path: str):
                 "heading_completion": last_heading.strip(),
                 "text": line.strip(),
                 "text_completion": line.strip(),
-                # "text_completion": {
-                #     "input": line.strip()
-                # },
+                "text_synonym": line.strip(),
+
             }
 
             if line not in['', '\n']:
                 client.index(index=index_name, body=doc, id=unique_id)
 
-def index_directory_to_elasticsearch():
+def index_directory_to_elasticsearch(client = elastic_client, 
+                                     index_name=elastic_index_name, 
+                                     raw_data=raw_data):
 
     glob_pattern = raw_data 
+
     ic("Indexing {}".format(glob_pattern))
     
     for file_path in glob.glob(glob_pattern, recursive=True):
         index_file_to_elasticsearch(file_path)
 
 if __name__ == "__main__":
-    create_index_with_fields()
-    index_directory_to_elasticsearch()
+
+    create_synonyms_with_csv(client = elastic_client, 
+                             synonyms_fn= elastic_synonym_fn, 
+                             synonyms_id=elastic_synonym_id)
+    create_index_with_fields(client = elastic_client, 
+                             index_name=elastic_index_name)
+    index_directory_to_elasticsearch(client=elastic_client, 
+                                     index_name=elastic_index_name, 
+                                     raw_data=raw_data)
+    
     ic(elastic_index_name, raw_data)
--- searchbox.py
+++ searchbox.py
@@ -12,28 +12,38 @@ elastic_cloud_id = config('ELASTIC_CLOUD_ID', default='none')
 elastic_api_key = config('ELASTIC_API_KEY', default='none')
 elastic_index_name = config('ELASTIC_INDEX_NAME', default='none')
 
+elastic_client = Elasticsearch(
+    cloud_id=elastic_cloud_id,
+    api_key=elastic_api_key,
+)
+
 def query_elastic_by_field(searchterm: str, 
                   index_name=elastic_index_name, 
                   field_name="",
-                  cloud_id=elastic_cloud_id, 
-                  api_key=elastic_api_key) -> List[Any]:
-    
-    client = Elasticsearch(
-        cloud_id=cloud_id,
-        api_key=api_key
-    )
+                  search_type="match",
+                  fuzziness: str = None,
+                  client=elastic_client) -> List[Any]:
 
-    search_body = {
-        "query": {
-            "match": { 
-                field_name : {
-                    "query": searchterm
-                }
+    query_body = {"query": {}}
+
+    if search_type == "match":
+        # using a match_phrase_prefix should allow this to use synonyms, but just prefixes
+        query_body["query"]["match"] = {
+            field_name : {
+                "query": searchterm
             }
         }
-    }
 
-    response = client.search(index=index_name, body=search_body)
+    elif search_type == "fuzzy":
+        query_body["query"]["fuzzy"] = {
+            field_name : {
+                "value": searchterm,
+                "fuzziness": fuzziness if fuzziness else "AUTO"
+            }
+        }
+
+
+    response = client.search(index=index_name, body=query_body)
     hits = response['hits']['hits']
 
     return hits
@@ -45,95 +55,95 @@ def search_elastic(searchterm: str) -> List[Any]:
     hits = query_elastic_by_field(searchterm, 
                                   index_name=elastic_index_name, 
                                   field_name=index_field_name,
-                                  cloud_id=elastic_cloud_id,
-                                  api_key=elastic_api_key)
+                                  search_type="match",
+                                  client=elastic_client)
 
     ic(hits)
 
-    text_values = [hit['_source']['text'] for hit in hits if '_source' in hit and 'text' in hit['_source']]
+    text_values = [hit['_source'][index_field_name] for hit in hits if '_source' in hit and index_field_name in hit['_source']]
 
     return text_values
 
-def suggest_elastic(searchterm: str) -> List[Any]:
 
-    index_field_name = "text_completion"
+def suggest_elastic(searchterm: str, field_name = "text_completion") -> List[Any]:
+
+    # index_field_name = "text_completion"
+    index_field_name = field_name
 
     suggestions = query_elastic_by_field(searchterm, 
                                         index_name=elastic_index_name, 
                                         field_name=index_field_name,
-                                        cloud_id=elastic_cloud_id,
-                                        api_key=elastic_api_key)
+                                        search_type="match",
+                                        client=elastic_client)
 
     text_values = [suggestion['_source']['text'] for suggestion in suggestions]
 
     return text_values
 
+def synonym_elastic(searchterm: str, field_name = "text_synonym") -> List[Any]:
+
+    index_field_name = field_name
+
+    hits = query_elastic_by_field(searchterm, 
+                                  index_name=elastic_index_name, 
+                                  field_name=index_field_name,
+                                  search_type="match",
+                                  client=elastic_client)
+
+    text_values = [hit['_source'][index_field_name] for hit in hits if '_source' in hit and index_field_name in hit['_source']]
+
+    return text_values
+
+
+def fuzzy_elastic(searchterm: str) -> List[Any]:
+
+    index_field_name = "text"
+
+    hits = query_elastic_by_field(searchterm, 
+                                  index_name=elastic_index_name, 
+                                  field_name=index_field_name,
+                                  search_type="fuzzy",
+                                  fuzziness=2,
+                                  client=elastic_client)
+
+    ic(hits)
+
+    text_values = [hit['_source'][index_field_name] for hit in hits if '_source' in hit and index_field_name in hit['_source']]
+
+    return text_values
+
+
+
 # function with list of labels
 def search_wikipedia(searchterm: str) -> List[any]:
     return wikipedia.search(searchterm) if searchterm else []
 
-# def search_elastic(searchterm: str) -> List[Any]:
+# pass search function to searchbox
+selected_value = st_searchbox(
+    search_elastic,
+    key="elastic_suggestbox",
+    label="Search Elastic",
+)
 
-#     cloud_id = elastic_cloud_id
-#     api_key = elastic_api_key
-    
-#     client = Elasticsearch(
-#         cloud_id=cloud_id,
-#         api_key=api_key
-#     )
-    
-#     search_body = {
-#         "query": {
-#             "match": { 
-#                 "text": {
-#                     "query": searchterm
-#                 }
-#             }
-#         }
-#     }
-    
-#     response = client.search(index=elastic_index_name, body=search_body)
-#     hits = response['hits']['hits']
-
-#     ic(hits)
-
-#     text_values = [hit['_source']['text'] for hit in hits if '_source' in hit and 'text' in hit['_source']]
-
-#     ic(elastic_index_name, search_body, text_values)
-#     return text_values
-
-# def suggest_elastic(searchterm: str) -> List[Any]:
-
-#     cloud_id = elastic_cloud_id
-#     api_key = elastic_api_key
-    
-#     client = Elasticsearch(
-#         cloud_id=cloud_id,
-#         api_key=api_key
-#     )
-
-#     search_body = {
-#         "query": {
-#             "match": { 
-#                 "text_completion": {
-#                     "query": searchterm
-#                 }
-#             }
-#         }
-#     }
-
-#     response = client.search(index=elastic_index_name, body=search_body)
-#     suggestions = response['hits']['hits']
-
-#     text_values = [suggestion['_source']['text'] for suggestion in suggestions]
-
-#     ic(elastic_index_name, search_body, text_values)
-#     return text_values
+# pass search function to searchbox
+selected_value = st_searchbox(
+    fuzzy_elastic,
+    key="elastic_fuzzybox",
+    label="Search Elastic (fuzzy)",
+)
 
 # pass search function to searchbox
 selected_value = st_searchbox(
     suggest_elastic,
     key="elastic_searchbox",
+    label="Search Elastic (with suggestions)",
+)
+
+# pass search function to searchbox
+selected_value = st_searchbox(
+    synonym_elastic,
+    key="elastic_synonymbox",
+    label="Search Elastic (with synonyms)",
 )
 
 # pass search function to searchbox
